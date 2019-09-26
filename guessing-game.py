4b69f2f1382b12f8a2792e25b3b606a811b0320d
Gavin Dale <gavin.dale@nextiva.com>
2019-09-25 22:17:31 -0700

    just a dumb game

---
+++ guessing-game.py
@@ -0,0 +1,26 @@
+import random
+
+targetNum = random.randint(1,100)
+print("pick a number between 1 and 100. Try to guess the number")
+guesses = []
+
+while True:
+    guess = int(input("Enter a number between 1 and 100: \n"))
+
+    if guess < 1 or guess > 100:
+        print("Please enter a number between 1 and 100: \n")
+        continue
+
+    elif guess == targetNum:
+        print("Wow! So good! You guessed it!")
+        break    
+    else:
+        guesses.append(guess)
+    
+    if guess > targetNum:
+        print("too high")
+    elif guess < targetNum:
+        print("too low")
+
+#for guess in guesses:
+#    if guess = 
